e1718c825a3b2edc6a9af297871773043dd856db
DiegoUX-UI <diego.lopez@dts.lat>
2024-07-09 17:49:59 -0400

    cambios y paginas agregadas

--- Plantilla/assets/js/main.js
+++ Plantilla/assets/js/main.js
@@ -312,19 +312,35 @@
 
 
 // Sidebar
+// let arrow = document.querySelectorAll(".arrow");
+// for (var i = 0; i < arrow.length; i++) {
+//   arrow[i].addEventListener("click", (e) => {
+//     let arrowParent = e.target.parentElement.parentElement;
+//     arrowParent.classList.toggle("showMenu");
+//   });
+// }
+// let sidebar = document.querySelector(".sidebar");
+// let sidebarBtn = document.querySelector(".toggle-sidebar-btn");
+// console.log(sidebarBtn);
+// sidebarBtn.addEventListener("click", () => {
+//   sidebar.classList.toggle("close");
+// });
+
 let arrow = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrow.length; i++) {
+for (let i = 0; i < arrow.length; i++) {
   arrow[i].addEventListener("click", (e) => {
     let arrowParent = e.target.parentElement.parentElement;
     arrowParent.classList.toggle("showMenu");
   });
 }
+
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".toggle-sidebar-btn");
-console.log(sidebarBtn);
+
 sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
+
 
 
 // document.addEventListener("DOMContentLoaded", () => {
